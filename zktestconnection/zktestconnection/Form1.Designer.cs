1541e0a26d2024e9b71f367dc5577c5bfcfa4a5f
Enes Aslan <ea.easlan@gmail.com>
2025-09-13 10:02:18 +0300

    last

---
+++ zktestconnection/zktestconnection/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace zktestconnection
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect and Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "192.168.0.172";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "4370";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1244, 546);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1261, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(287, 546);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Excel Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1414, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1526, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1261, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Clear Log";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IP :";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1554, 599);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ZKTeco Log Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ zktestconnection/zktestconnection/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using zkemkeeper;
+using ClosedXML.Excel;
+using System.IO;
+
+namespace zktestconnection
+{
+    public partial class Form1 : Form
+    {
+        private CZKEM cZKEM;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // pencere sabit boyut
+            this.MaximizeBox = false;
+            richTextBox1.Clear();
+            dataGridView1.Columns.Clear();
+            try
+            {
+                cZKEM = new CZKEM();
+                string sdkVersion = "";
+
+                if (cZKEM.GetSDKVersion(ref sdkVersion))
+                {
+                    label1.Text = "SDK Version: " + sdkVersion;
+                }
+                else
+                {
+                    label1.Text = "SDK Version could not read";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ip = textBox1.Text.Trim();
+            if (!int.TryParse(textBox2.Text.Trim(), out int port))
+            {
+                MessageBox.Show("Please enter a valid port number");
+                return;
+            }
+
+            LogEkle($"Connecting... IP: {ip}, Port: {port}");
+
+            try
+            {
+                cZKEM = (CZKEM)Activator.CreateInstance(
+                    Type.GetTypeFromCLSID(new Guid("00853A19-BD51-419B-9269-2DABE57EB61F")));
+
+                if (!cZKEM.Connect_Net(ip, port))
+                {
+                    LogEkle("Oppps! Something Went Wrong");
+                    MessageBox.Show("Could not connect to the device!");
+                    return;
+                }
+
+                LogEkle("Connection successful");
+                int machineNumber = 1;
+
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("EnrollNumber", "User ID");
+                dataGridView1.Columns.Add("VerifyMode", "VerifyMode");
+                dataGridView1.Columns.Add("InOutMode", "InOutMode");
+                dataGridView1.Columns.Add("DateTime", "DateTime");
+                dataGridView1.Columns.Add("WorkCode", "WorkCode");
+                dataGridView1.Columns.Add("Name", "Name");
+                dataGridView1.Columns.Add("Password", "Password");
+                dataGridView1.Columns.Add("Privilege", "Privilege");
+                dataGridView1.Columns.Add("Enabled", "EnabledDisabled");
+                dataGridView1.Columns.Add("FingerIndex", "FingerIndex");
+                dataGridView1.Columns.Add("TmpLength", "TmpLength");
+                dataGridView1.Columns.Add("Flag", "Flag");
+
+                // Kullanıcı bilgilerini dictionary içine al
+                Dictionary<string, (string name, string password, int privilege, bool enabled)> userInfoDict
+                    = new Dictionary<string, (string, string, int, bool)>();
+
+                string enrollNumber, name, password;
+                int privilege;
+                bool enabled;
+
+                if (cZKEM.ReadAllUserID(machineNumber))
+                {
+                    while (cZKEM.SSR_GetAllUserInfo(machineNumber, out enrollNumber, out name, out password, out privilege, out enabled))
+                    {
+                        if (!userInfoDict.ContainsKey(enrollNumber))
+                        {
+                            userInfoDict[enrollNumber] = (name, password, privilege, enabled);
+                        }
+                    }
+                }
+
+                // Logları oku
+                if (cZKEM.ReadAllGLogData(machineNumber))
+                {
+                    dataGridView1.Rows.Clear();
+                    int workCode = 0;
+                    int verifyMode, inOutMode;
+                    int year, month, day, hour, minute, second;
+                    int satirSayisi = 0;
+
+                    while (cZKEM.SSR_GetGeneralLogData(
+                        machineNumber,
+                        out enrollNumber,
+                        out verifyMode,
+                        out inOutMode,
+                        out year, out month, out day,
+                        out hour, out minute, out second,
+                        ref workCode))
+                    {
+                        string tarihSaat = $"{year}-{month:D2}-{day:D2} {hour:D2}:{minute:D2}:{second:D2}";
+
+                        // Kullanıcı bilgilerini dictionary’den al
+                        string uName = "", uPass = "";
+                        string uPriv = "", uEnabled = "";
+
+                        if (userInfoDict.ContainsKey(enrollNumber))
+                        {
+                            var info = userInfoDict[enrollNumber];
+                            uName = info.name;
+                            uPass = info.password;
+                            uPriv = info.privilege.ToString();
+                            uEnabled = info.enabled ? "Yes" : "No";
+                        }
+
+                        int fingerIndex = -1;
+                        int flag = 0;
+                        int tmpLength = 0;
+                        string tmpData;
+                        if (cZKEM.GetUserTmpExStr(machineNumber, enrollNumber, 0, out flag, out tmpData, out tmpLength))
+                        {
+                            fingerIndex = 0;
+                        }
+
+                        // Satırı ekle
+                        dataGridView1.Rows.Add(
+                            enrollNumber,
+                            verifyMode,
+                            inOutMode,
+                            tarihSaat,
+                            workCode,
+                            uName,
+                            uPass,
+                            uPriv,
+                            uEnabled,
+                            fingerIndex == -1 ? "" : fingerIndex.ToString(),
+                            tmpLength == 0 ? "" : tmpLength.ToString(),
+                            flag.ToString()
+                        );
+
+                        satirSayisi++;
+                    }
+
+                    LogEkle($"Total of {satirSayisi} log rows were retrieved.");
+                }
+                else
+                {
+                    LogEkle("Log data could not be read.");
+                    MessageBox.Show("Log data could not be read!");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogEkle("Error: " + ex.Message);
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void LogEkle(string mesaj)
+        {
+            richTextBox1.AppendText($"[{DateTime.Now:HH:mm:ss}] {mesaj}\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("No data found to export.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel |*.xlsx";
+            saveFileDialog.Title = "Excel";
+            saveFileDialog.FileName = "log.xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var workbook = new XLWorkbook())
+                    {
+                        var worksheet = workbook.Worksheets.Add("Logs");
+
+                        // Başlıklar
+                        for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                        {
+                            worksheet.Cell(1, i + 1).Value = dataGridView1.Columns[i].HeaderText;
+                        }
+
+                        // Satırlar
+                        for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                        {
+                            for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                            {
+                                worksheet.Cell(i + 2, j + 1).Value = dataGridView1.Rows[i].Cells[j].Value?.ToString();
+                            }
+                        }
+
+                        workbook.SaveAs(saveFileDialog.FileName);
+                    }
+
+                    MessageBox.Show("Excel file has been successfully created!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("• It can read data from ZKTeco branded devices and export it\n\n• It works with TCP protocol\n\n\n Created By Enes Aslan | 2025", "ZktEco Data Fetcher");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+    }
+}
